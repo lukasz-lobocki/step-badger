eeab474b64e4102085b31078389f52b3526fe7a6
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2024-07-10 21:44:58 +0200

    chore: wip.

--- cmd/export_defs.go
+++ cmd/export_defs.go
@@ -1,8 +1,30 @@
 package cmd
 
+import (
+	"crypto/x509"
+	"time"
+)
+
 /*
 Configuration structure for exportCmd
 */
 type tConfig struct {
 	emitFormat *tChoice
+}
+
+type tCertificate struct {
+	Cert       x509.Certificate       `json:"topLevelPath"` // Full path
+	Revocation tCertificateRevocation `json:"uniqueName"`   // Shortest unique path
+}
+
+type tCertificateRevocation struct {
+	Serial        string    `json:"Serial"`
+	ProvisionerID string    `json:"ProvisionerID"`
+	ReasonCode    int       `json:"ReasonCode"`
+	Reason        string    `json:"Reason"`
+	RevokedAt     time.Time `json:"RevokedAt"`
+	ExpiresAt     time.Time `json:"ExpiresAt"`
+	TokenID       string    `json:"TokenID"`
+	MTLS          bool      `json:"MTLS"`
+	ACME          bool      `json:"ACME"`
 }
