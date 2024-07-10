b5ee6db335f8dacbb797dc829c4e0ea604ced08f
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2024-07-10 09:56:21 +0200

    build: Add cobra.

--- main.go
+++ main.go
@@ -1,50 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-var (
-	semVer      string
-	commitHash  string
-	isGitDirty  string
-	isSnapshot  string
-	goOs        string
-	goArch      string
-	gitUrl      string
-	builtBranch string
-	builtDate   string
-)
-
-var semReleaseVersion string = semVer +
-	func(pre string, txt string) string {
-		if len(txt) > 0 {
-			return pre + txt
-		} else {
-			return ""
-		}
-	}("+", goArch) +
-	func(pre string, txt string) string {
-		if len(txt) > 0 {
-			return pre + txt
-		} else {
-			return ""
-		}
-	}(".", builtBranch) +
-	func(pre string, txt string) string {
-		if len(txt) > 0 {
-			return pre + txt
-		} else {
-			return ""
-		}
-	}(".", commitHash)
+import "github.com/lukasz-lobocki/step-badger/cmd"
 
 func main() {
-	println("release version:", semReleaseVersion)
-	println()
-	println("semVer:", semVer)
-	println("commitHash:", commitHash)
-	println("isGitDirty:", isGitDirty)
-	println("isSnapshot:", isSnapshot)
-	println("goOs:", goOs)
-	println("goArch:", goArch)
-	println("gitUrl:", gitUrl)
-	println("builtBranch:", builtBranch)
-	println("builtDate:", builtDate)
+	cmd.Execute()
 }
